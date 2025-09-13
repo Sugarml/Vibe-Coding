cfe1090f196a0bf0dad5eecc4152b2535d0499dd
Sugarml <acup0312@gmail.com>
2025-09-13 01:25:04 +0000

    package增加module

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-
+// https://vite.dev/config/
 export default defineConfig({
-plugins: [react()],
-base: '/blog-app/' // 這裡要改成你的 GitHub repo 名稱
-});
+  plugins: [react()],
+  base: './',
+})
